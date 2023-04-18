df215b7c098564e5a53f771aea7a76878ab2e316
Jimmy <jimmyzhang1@hotmail.com>
2023-04-18 13:42:10 -0700

    Implement dotenv to protect sensitive information regarding postgreSQL database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
+const db = require('./queries');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
+app.post('/users', db.createUser);
+
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+  console.log(`AiSpy Express running on port ${port}.`);
 });
--- backend/queries.js
+++ backend/queries.js
@@ -6,3 +6,18 @@ const pool = new Pool({
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
 });
+
+const createUser = (req, res) => {
+  const { name, email } = request.body;
+
+  pool.query('INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+  });
+};
+
+module.exports = {
+  createUser
+};
