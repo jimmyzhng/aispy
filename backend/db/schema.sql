797c3291c8374fefa966f121529e442261757657
Jimmy <jimmyzhang1@hotmail.com>
2023-04-25 15:08:43 -0700

    Add AWS S3 connection

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,4 +1,5 @@
   DROP TABLE IF EXISTS users CASCADE;
+  DROP TABLE IF EXISTS videos CASCADE;
 
   CREATE TABLE users (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -6,4 +7,13 @@
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 
+  );
+
+  CREATE TABLE videos (
+    id INT PRIMARY KEY NOT NULL,
+    url VARCHAR(255),
+    date DATE,
+    building VARCHAR(255),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
   );
--- backend/db/seeds.sql
+++ backend/db/seeds.sql
@@ -1,2 +1,11 @@
 INSERT INTO users (username, email, password)
-VALUES ('jimmy', 'jimmy@test.ca', '123');
+VALUES ('user1', 'user@test.ca', '123');
+
+INSERT INTO videos (url, date, building, user_id)
+VALUES 
+('https://aispy.s3.us-west-2.amazonaws.com/pb-north.mp4','2023-04-23', 'North', 1),
+('https://aispy.s3.us-west-2.amazonaws.com/pb-south.mp4','2023-04-24', 'South', 1),
+('https://aispy.s3.us-west-2.amazonaws.com/pb-west.mp4', '2023-04-25', 'West', 1);
+
+
+
